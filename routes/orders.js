10940fc4eee2469168f9a0982f1d504006cc2684
Faycel Bousalhi <faycel.bouslahi@me.com>
2022-12-17 21:58:38 +0100

    Add Orders Routers

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,9 @@
 const { Order } = require('../models/order');
+const { OrderItem } = require('../models/order-item');
 const express = require('express');
 const router = express.Router();
 
+//Get a list of Orders
 router.get(`/`, async (req, res) => {
     const orderList = await Order.find();
 
@@ -10,5 +12,84 @@ router.get(`/`, async (req, res) => {
     }
     res.send(orderList);
 })
+
+
+//To DO:
+//Get Order Detail and Populate Products in Order Items and User Data
+
+
+//Create a New Order
+router.post('/', async (req, res) => {
+
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) => {
+        let newOrderItem = new OrderItem({
+            quantity: orderItem.quantity,
+            product: orderItem.product
+        })
+
+        newOrderItem = await newOrderItem.save();
+
+        return newOrderItem._id;
+    }))
+    const orderItemsIdsResolved = await orderItemsIds;
+
+    //To DO :
+    // Total price
+
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        user: req.body.user
+    })
+    order = await order.save();
+
+    if (!order)
+        return res.status(400).send('the oreder cannot be created');
+
+    res.send(order);
+})
+
+//Update Order Status
+router.put('/:id', async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status
+        },
+        { new: true }
+    )
+
+    if (!order)
+        return res.status(400).send('the order cannot be update!')
+
+    res.send(order);
+})
+
+//Delete Order
+router.delete('/:id', (req, res) => {
+    Order.findByIdAndRemove(req.params.id).then(async order => {
+        if (order) {
+            await order.orderItems.map(async orderItem => {
+                await OrderItem.findByIdAndRemove(orderItem)
+            })
+            return res.status(200).json({ success: true, message: 'the order is deleted!' })
+        } else {
+            return res.status(404).json({ success: false, message: "order not found!" })
+        }
+    }).catch(err => {
+        return res.status(500).json({ success: false, error: err })
+    })
+})
+
+//To DO:
+//Get Total E-Shop Sales
+//Get User Orders
 
 module.exports = router;
